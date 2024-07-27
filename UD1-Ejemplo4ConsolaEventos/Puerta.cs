ce3d92ae29aa63be5451e7b29a0923ebb56e3d0c
i12laroa <63612440+i12laroa@users.noreply.github.com>
2024-07-27 17:30:14 +0200

    Agregar archivos de proyecto.

---
+++ UD1-Ejemplo4ConsolaEventos/Puerta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD1_Ejemplo4ConsolaEventos
+{
+    internal class Puerta
+    {
+        public string Location { get; set; }
+
+        // Definir el evento usando EventHandler
+        public event EventHandler<PuertaEventArgs> PuertaAbierta;
+
+        public Puerta(string location)
+        {
+            Location = location;
+        }
+
+        public void Abierta()
+        {
+            Console.WriteLine($"Puerta del {Location} está abierta.");
+            OnPuertaAbierta(new PuertaEventArgs(Location));
+        }
+
+        protected virtual void OnPuertaAbierta(PuertaEventArgs e)
+        {
+            PuertaAbierta?.Invoke(this, e);
+        }
+    }
+}
---
+++ UD1-Ejemplo4ConsolaEventos/PuertaEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD1_Ejemplo4ConsolaEventos
+{
+    internal class PuertaEventArgs : EventArgs
+    {
+        public string Location { get; set; }
+
+        public PuertaEventArgs(string location)
+        {
+            Location = location;
+        }
+    }
+}
